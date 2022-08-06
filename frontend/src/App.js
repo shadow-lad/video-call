beedbc350821d01ccea6dd0fd8164181f546b1f1
shadow-lad <shardav.lad21@gmail.com>
2022-08-06 15:55:46 +0530

    Implemented dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import { Navigate, Route, Routes } from "react-router";
-import Nav from "./components/common/Nav";
 import Home from "./components/Home";
 import Room from "./components/Room";
 
 function App() {
 	return (
 		<>
-			<Nav />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/room/:roomid" element={<Room />} />
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,22 +1,29 @@
 function Home() {
 	return (
-		<div className="center-container flex-column">
-			<h1 className="text-xl">
-				Let's join a <span className="accent">room!</span>
-			</h1>
-			<div className="room-input-container">
-				<form method="POST" action="/api/create-room">
-					<button type="submit" className="btn btn-accent">
-						Create a Room
-					</button>
-				</form>
-				<h2>OR</h2>
-				<div className="input-group">
-					<input className="input-control" type="text" placeholder="Enter Code" />
-					<button className="btn btn-outline-accent">Join</button>
+		<>
+			<div className="center-container flex-column">
+				<h1 className="text-xl">
+					Let's join a <span className="accent">room!</span>
+				</h1>
+				<div className="room-input-container">
+					<form method="POST" action="/api/create-room">
+						<button type="submit" className="btn btn-accent">
+							Create a Room
+						</button>
+					</form>
+					<h2>OR</h2>
+					<div className="input-group">
+						<input className="input-control" type="text" placeholder="Enter Code" />
+						<button className="btn btn-outline-accent">Join</button>
+					</div>
 				</div>
 			</div>
-		</div>
+			<footer>
+				<small>
+					Created by <a href="https://github.com/shadow-lad">theshadowlad</a>
+				</small>
+			</footer>
+		</>
 	);
 }
 
